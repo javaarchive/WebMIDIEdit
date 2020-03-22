1faeb129d48198c2e085ccd2fd24314898e2f8cf
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-03-21 18:19:16 -0700

    Update midiedit.js

--- midiedit.js
+++ midiedit.js
@@ -1,5 +1,5 @@
 console.log("Loading Midi Editor");
 function authorizemidi(){
-  navigator.requestMIDIAccess().then(function(info){
-  }).err(console.log);
+  window.navigator.requestMIDIAccess().then(function(info){
+  }).catch(console.log);
 }
